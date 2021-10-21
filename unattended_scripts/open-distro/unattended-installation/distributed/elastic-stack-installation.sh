132dfcbd71a983c17fa7aebb9d717f510cbb8bbd
verd-x <verdx@riseup.net>
2021-10-21 17:49:33 +0200

    Corrected logger usage

--- unattended_scripts/open-distro/unattended-installation/distributed/elastic-stack-installation.sh
+++ unattended_scripts/open-distro/unattended-installation/distributed/elastic-stack-installation.sh
@@ -39,7 +39,7 @@ checkArch() {
     arch=$(uname -m)
 
     if [ ${arch} != "x86_64" ]; then
-        echo "Uncompatible system. This script must be run on a 64-bit system."
+        logger "Uncompatible system. This script must be run on a 64-bit system."
         exit 1;
     fi
 }
@@ -51,31 +51,31 @@ startService() {
         eval "systemctl enable $1.service ${debug}"
         eval "systemctl start $1.service ${debug}"
         if [  "$?" != 0  ]; then
-            echo "${1^} could not be started."
+            logger "${1^} could not be started."
             exit 1;
         else
-            echo "${1^} started"
+            logger "${1^} started"
         fi
     elif [ -n "$(ps -e | egrep ^\ *1\ .*init$)" ]; then
         eval "chkconfig $1 on ${debug}"
         eval "service $1 start ${debug}"
         eval "/etc/init.d/$1 start ${debug}"
         if [  "$?" != 0  ]; then
-            echo "${1^} could not be started."
+            logger "${1^} could not be started."
             exit 1;
         else
-            echo "${1^} started"
+            logger "${1^} started"
         fi
     elif [ -x /etc/rc.d/init.d/$1 ] ; then
         eval "/etc/rc.d/init.d/$1 start ${debug}"
         if [  "$?" != 0  ]; then
-            echo "${1^} could not be started."
+            logger "${1^} could not be started."
             exit 1;
         else
-            echo "${1^} started"
+            logger "${1^} started"
         fi
     else
-        echo "Error: ${1^} could not start. No service manager found on the system."
+        logger "Error: ${1^} could not start. No service manager found on the system."
         exit 1;
     fi
 }
@@ -99,17 +99,17 @@ getHelp() {
 checkConfig() {
 
     if [ -f ~/config.yml ]; then
-        echo "Configuration file found. Starting the installation..."
+        logger "Configuration file found. Starting the installation..."
     else
         if [ -f ~/certs.tar ]; then
-            echo "Certificates file found. Starting the installation..."
+            logger "Certificates file found. Starting the installation..."
             eval "tar --overwrite -C ~/ -xf ~/certs.tar config.yml ${debug}"
         elif [ -f /etc/elasticsearch/certs/certs.tar ]; then
             eval "mv /etc/elasticsearch/certs/certs.tar ~/ ${debug}"
             eval "tar --overwrite -C ~/ -xf ~/certs.tar config.yml ${debug}"
-            echo "Certificates file found. Starting the installation..."
+            logger "Certificates file found. Starting the installation..."
         else
-            echo "No configuration file found."
+            logger "No configuration file found."
             exit 1;
         fi
     fi
@@ -180,7 +180,7 @@ installPrerequisites() {
     fi
 
     if [  "$?" != 0  ]; then
-        echo "Error: Prerequisites could not be installed"
+        logger "Error: Prerequisites could not be installed"
         exit 1;
     else
         logger "Done"
@@ -210,7 +210,7 @@ addWazuhrepo() {
 installElasticsearch() {
 
     if [[ -f /etc/elasticsearch/elasticsearch.yml ]]; then
-        echo "Open Distro for Elasticsearch is already installed in this node."
+        logger "Open Distro for Elasticsearch is already installed in this node."
         exit 1;
     fi
 
@@ -225,7 +225,7 @@ installElasticsearch() {
     fi
 
     if [  "$?" != 0  ]; then
-        echo "Error: Elasticsearch installation failed"
+        logger "Error: Elasticsearch installation failed"
         exit 1;
     else
         logger "Done"
@@ -279,7 +279,7 @@ installElasticsearch() {
                 fi
             done
             if [[ ! " ${IMN[@]} " =~ " ${iname} " ]]; then
-                echo "The name given does not appear on the configuration file"
+                logger "The name given does not appear on the configuration file"
                 exit 1;
             fi
             nip="${DSH[pos]}"
@@ -332,7 +332,7 @@ installElasticsearch() {
             copyCertificates iname pos
         fi
         initializeElastic nip
-        echo "Done"
+        logger "Done"
     fi
 }
 
@@ -372,7 +372,7 @@ createCertificates() {
     eval "chmod +x ~/searchguard/tools/sgtlstool.sh ${debug}"
     eval "bash ~/searchguard/tools/sgtlstool.sh -c ~/searchguard/search-guard.yml -ca -crt -t /etc/elasticsearch/certs/ ${debug}"
     if [  "$?" != 0  ]; then
-        echo "Error: certificates were not created"
+        logger "Error: certificates were not created"
         exit 1;
     else
         logger "Certificates created"
@@ -436,7 +436,7 @@ initializeElastic() {
 installKibana() {
 
     if [[ -f /etc/kibana/kibana.yml ]]; then
-        echo "Kibana is already installed in this node."
+        logger "Kibana is already installed in this node."
         exit 1;
     fi
 
@@ -447,7 +447,7 @@ installKibana() {
         eval "${sys_type} install opendistroforelasticsearch-kibana${sep}${OD_VER} -y -q ${debug}"
     fi
     if [  "$?" != 0  ]; then
-        echo "Error: Kibana installation failed"
+        logger "Error: Kibana installation failed"
         exit 1;
     else
         eval "curl -so /etc/kibana/kibana.yml https://packages.wazuh.com/resources/${WAZUH_MAJOR}/open-distro/unattended-installation/distributed/templates/kibana_unattended.yml --max-time 300 ${debug}"
@@ -456,7 +456,7 @@ installKibana() {
         eval "cd /usr/share/kibana ${debug}"
         eval "sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-${WAZUH_VER}_${ELK_VER}-${WAZUH_KIB_PLUG_REV}.zip ${debug}"
         if [  "$?" != 0  ]; then
-            echo "Error: Wazuh Kibana plugin could not be installed."
+            logger "Error: Wazuh Kibana plugin could not be installed."
             exit 1;
         fi
         eval "setcap 'cap_net_bind_service=+ep' /usr/share/kibana/node/bin/node ${debug}"
@@ -514,7 +514,7 @@ copyKibanacerts() {
         #     eval "mv /etc/kibana/certs/${iname}_http.key /etc/kibana/certs/kibana.key ${debug}"
         # fi            
     else
-        echo "No certificates found. Could not initialize Kibana"
+        logger "No certificates found. Could not initialize Kibana"
         exit 1;
     fi
 
@@ -534,7 +534,7 @@ initializeKibana() {
     wip="${wip//$rm}"
     conf="$(awk '{sub("url: https://localhost", "url: https://'"${wip}"'")}1' /usr/share/kibana/data/wazuh/config/wazuh.yml)"
     echo "${conf}" > /usr/share/kibana/data/wazuh/config/wazuh.yml  
-    echo $'\nYou can access the web interface https://'${kip}'. The credentials are admin:admin'    
+    logger $'\nYou can access the web interface https://'${kip}'. The credentials are admin:admin'    
 
 }
 
@@ -555,17 +555,17 @@ healthCheck() {
     ram_gb=$(free -m | awk '/^Mem:/{print $2}')
     if [ -n "${elastic}" ]; then
         if [ ${cores} -lt 2 ] || [ ${ram_gb} -lt 3700 ]; then
-            echo "Your system does not meet the recommended minimum hardware requirements of 4Gb of RAM and 2 CPU cores. If you want to proceed with the installation use the -i option to ignore these requirements."
+            logger "Your system does not meet the recommended minimum hardware requirements of 4Gb of RAM and 2 CPU cores. If you want to proceed with the installation use the -i option to ignore these requirements."
             exit 1;
         else
-            echo "Starting the installation..."
+            logger "Starting the installation..."
         fi
     elif [ -n "${kibana}" ]; then
         if [ ${cores} -lt 2 ] || [ ${ram_gb} -lt 3700 ]; then
-            echo "Your system does not meet the recommended minimum hardware requirements of 4Gb of RAM and 2 CPU cores. If you want to proceed with the installation use the -i option to ignore these requirements."
+            logger "Your system does not meet the recommended minimum hardware requirements of 4Gb of RAM and 2 CPU cores. If you want to proceed with the installation use the -i option to ignore these requirements."
             exit 1;
         else
-            echo "Starting the installation..."
+            logger "Starting the installation..."
         fi
     fi
 
@@ -613,7 +613,7 @@ main() {
         done
 
         if [ "$EUID" -ne 0 ]; then
-            echo "This script must be run as root."
+            logger "This script must be run as root."
             exit 1;
         fi
 
@@ -638,7 +638,7 @@ main() {
         if [ -n "${elastic}" ]; then
 
             if [ -n "${ignore}" ]; then
-                echo "Health-check ignored."
+                logger "Health-check ignored."
             else
                 healthCheck elastic
             fi
@@ -651,7 +651,7 @@ main() {
         if [ -n "${kibana}" ]; then
 
             if [ -n "${ignore}" ]; then
-                echo "Health-check ignored."
+                logger "Health-check ignored."
             else
                 healthCheck kibana
             fi
